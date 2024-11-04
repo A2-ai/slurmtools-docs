86877861b7d69bf005a5a69341bf6a87757097b8
matt smith <msmith21794@gmail.com>
2024-11-04 15:50:47 -0500

    initial shell of doc site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,57 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Slurmtools',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/A2-ai/slurmtools',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+				  label: 'User Guide',
+				  // introduce slurmtools and describe what it does
+				  items: [
+					{ label: 'Introduction', slug: 'functions/0_intro'},
+					// introduce submit functionality and how the functions work together
+					{
+						label: 'Submitting a job',
+						items: [
+						  { label: '1. Submitting a nonmem job', slug: 'functions/submit_nonmem_model' }, 
+						  { label: '1.1. Getting slurm partitions', slug: 'functions/get_slurm_partitions' },
+						  { label: '1.2. Generating nmm config', slug: 'functions/generate_nmm_config' },
+						],
+					  },
+					// introduce monitoring functionality  
+					{ 
+						label: 'Monitoring submitted jobs', 
+						items: [
+							{label: '2. Monitoring submitted jobs', slug: 'functions/get_slurm_jobs' },
+						],
+					},
+					// Introduce stopping functionality.
+					{
+						label: "Stopping a job",
+						items: [
+							{label: '3. Stopping a job', slug: 'functions/cancel_job'},
+						],
+					},
+					// Introduce expanding job templates
+					{ 
+						label: 'Expanding Job Templates',
+						items: [
+						  { label: '4. Adding notifications', slug: 'functions/simple_notifications' },
+						],
+					},
+				  ],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+				  label: 'Examples',
+				  autogenerate: { directory: 'examples' },
 				},
-			],
+				{
+				  label: 'Slurmtools Reference',
+				  link: 'https://a2-ai.github.io/slurmtools/' ,
+				},
+			  ],
 		}),
 	],
 });
