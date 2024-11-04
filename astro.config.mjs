71640fe5a98d7966ef7b2dffc70d001ce95db0a5
matt smith <msmith21794@gmail.com>
2024-11-04 16:56:06 -0500

    updating docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,41 +12,48 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-				  label: 'User Guide',
+				  label: 'Tutorials',
 				  // introduce slurmtools and describe what it does
 				  items: [
-					{ label: 'Introduction', slug: 'functions/0_intro'},
-					// introduce submit functionality and how the functions work together
+					{ label: 'Introduction', slug: 'tutorials/0_intro'},
+					// introduce submit functionality and how the tutorials work together
 					{
 						label: 'Submitting a job',
 						items: [
-						  { label: '1. Submitting a nonmem job', slug: 'functions/submit_nonmem_model' }, 
-						  { label: '1.1. Getting slurm partitions', slug: 'functions/get_slurm_partitions' },
-						  { label: '1.2. Generating nmm config', slug: 'functions/generate_nmm_config' },
+						  { label: '1. How to submit a NONMEM job to slurm', slug: 'tutorials/submit_nonmem_model' }, 
+						  { label: '1.1. How to see available partitions', slug: 'tutorials/get_slurm_partitions' },
+						  // This should go in a separate section about nmm I think
+						  { label: '1.2. How to generate a nmm config file', slug: 'tutorials/generate_nmm_config' },
 						],
 					  },
 					// introduce monitoring functionality  
 					{ 
 						label: 'Monitoring submitted jobs', 
 						items: [
-							{label: '2. Monitoring submitted jobs', slug: 'functions/get_slurm_jobs' },
+							{label: '2. How to see submitted jobs', slug: 'tutorials/get_slurm_jobs' },
 						],
 					},
 					// Introduce stopping functionality.
 					{
 						label: "Stopping a job",
 						items: [
-							{label: '3. Stopping a job', slug: 'functions/cancel_job'},
+							{label: '3. How to cancel a job', slug: 'tutorials/cancel_job'},
 						],
 					},
 					// Introduce expanding job templates
 					{ 
 						label: 'Expanding Job Templates',
 						items: [
-						  { label: '4. Adding notifications', slug: 'functions/simple_notifications' },
+						  { label: '4. How to get notifications', slug: 'tutorials/simple_notifications' },
 						],
 					},
 				  ],
+				},
+				{
+				  label: 'Guides',
+				  items: [
+					{ label: 'Deciding which partition to use', slug: 'guides/submit' },
+				  ]
 				},
 				{
 				  label: 'Examples',
