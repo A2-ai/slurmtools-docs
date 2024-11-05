7eae11b99e70da8a20c6621d4095d91418c4ab29
matt smith <msmith21794@gmail.com>
2024-11-05 16:43:32 -0500

    updated shell and started writing docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,59 +6,60 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Slurmtools',
+			title: 'slurmtools',
 			social: {
 				github: 'https://github.com/A2-ai/slurmtools',
 			},
 			sidebar: [
 				{
-				  label: 'Tutorials',
+				  label: 'User Guides',
 				  // introduce slurmtools and describe what it does
 				  items: [
-					{ label: 'Introduction', slug: 'tutorials/0_intro'},
+					{ label: 'Introduction', slug: 'guides/0_intro'},
+					{ label: 'Getting started', slug: 'guides/1_getting_started' },
 					// introduce submit functionality and how the tutorials work together
 					{
 						label: 'Submitting a job',
 						items: [
-						  { label: '1. How to submit a NONMEM job to slurm', slug: 'tutorials/submit_nonmem_model' }, 
-						  { label: '1.1. How to see available partitions', slug: 'tutorials/get_slurm_partitions' },
+						  { label: '1. How to submit a NONMEM job to slurm', slug: 'guides/submit_nonmem_model' }, 
+						  { label: '1.1. How to see available partitions', slug: 'guides/get_slurm_partitions' },
 						  // This should go in a separate section about nmm I think
-						  { label: '1.2. How to generate a nmm config file', slug: 'tutorials/generate_nmm_config' },
+						  { label: '1.2. How to generate a nmm config file', slug: 'guides/generate_nmm_config' },
 						],
 					  },
 					// introduce monitoring functionality  
 					{ 
 						label: 'Monitoring submitted jobs', 
 						items: [
-							{label: '2. How to see submitted jobs', slug: 'tutorials/get_slurm_jobs' },
+							{label: '2. How to see submitted jobs', slug: 'guides/get_slurm_jobs' },
 						],
 					},
 					// Introduce stopping functionality.
 					{
 						label: "Stopping a job",
 						items: [
-							{label: '3. How to cancel a job', slug: 'tutorials/cancel_job'},
+							{label: '3. How to cancel a job', slug: 'guides/cancel_job'},
 						],
 					},
 					// Introduce expanding job templates
 					{ 
 						label: 'Expanding Job Templates',
 						items: [
-						  { label: '4. How to get notifications', slug: 'tutorials/simple_notifications' },
+						  { label: '4. How to get notifications', slug: 'guides/simple_notifications' },
 						],
 					},
 				  ],
 				},
 				{
-				  label: 'Guides',
+				  label: 'Blogs',
 				  items: [
-					{ label: 'Deciding which partition to use', slug: 'guides/submit' },
+					{ label: 'Deciding which partition to use', slug: 'blogs/submit' },
 				  ]
 				},
-				{
-				  label: 'Examples',
-				  autogenerate: { directory: 'examples' },
-				},
+				//{
+				//  label: 'Examples',
+				//  autogenerate: { directory: 'examples' },
+				//},
 				{
 				  label: 'Slurmtools Reference',
 				  link: 'https://a2-ai.github.io/slurmtools/' ,
