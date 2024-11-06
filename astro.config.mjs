23aee10b0626bbf90590b6dc8b3fc7901b9044d9
matt smith <msmith21794@gmail.com>
2024-11-06 15:45:49 -0500

    updated base for site deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://crispy-guacamole-2k6epe2.pages.github.io/',
-	base: 'slurmtools-docs',
+	base: '/slurmtools-docs',
 	integrations: [
 		starlight({
 			title: 'slurmtools',
