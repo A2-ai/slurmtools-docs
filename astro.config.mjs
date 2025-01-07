78031166fe65c22ac9ee97eb7e26ac48d528d9bc
matt smith <msmith21794@gmail.com>
2025-01-07 16:18:44 -0500

    removed old site name from astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://crispy-guacamole-2k6epe2.pages.github.io/',
 	integrations: [
 		starlight({
 			title: 'slurmtools',
