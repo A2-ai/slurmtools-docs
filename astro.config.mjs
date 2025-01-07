7c28c43176ec82003965a596f805570cc882a798
matt smith <msmith21794@gmail.com>
2025-01-07 16:28:47 -0500

    updating astro.config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/slurmtools-docs', // Add this line
-  	site: 'https://a2-ai.github.io',
+  	site: 'https://a2-ai.github.io/slurmtools-docs'
 	integrations: [
 		starlight({
 			title: 'slurmtools',
